3388f8f33c6224d111b4fb326eb9292d88ce2fed
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-18 16:08:20 +0900

    MusicViewModelがPlayerScrrenに渡るように変更

--- app/src/main/java/com/example/music_player/PlayerActivity.kt
+++ app/src/main/java/com/example/music_player/PlayerActivity.kt
@@ -3,13 +3,15 @@ package com.example.music_player
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val MusicViewModel: MusicViewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
         setContent {
-            PlayerScreen()
+            PlayerScreen(MusicViewModel)
         }
     }
 }
