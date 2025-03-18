c2f4714785b3c47a7234b1a74d93d493c26b40f8
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-18 15:23:37 +0900

    Add function from PlayerScreen

--- app/src/main/java/com/example/music_player/PlayerScreen.kt
+++ app/src/main/java/com/example/music_player/PlayerScreen.kt
@@ -1,4 +1,55 @@
 package com.example.music_player
 
-class PlayerScreen {
+import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.material3.Surface
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PlayerScreen(viewModel: MusicViewModel = viewModel()) {
+    val currentSong by viewModel.currentSong.collectAsState()
+
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "再生中",
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = currentSong.title,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = currentSong.artist,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
 }
