2a53402ddde4882a0e7fb094b209f8f97d54319c
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-18 21:13:18 +0900

    ViewModelProvider.Factory を使って MusicViewModel に Context を渡せるように追加

--- app/src/main/java/com/example/music_player/MusicViewModelFactory.kt
+++ app/src/main/java/com/example/music_player/MusicViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.music_player
 
-class MusicViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MusicViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MusicViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MusicViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
