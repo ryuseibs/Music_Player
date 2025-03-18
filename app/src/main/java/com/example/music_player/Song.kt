c41cd0b75bac5a3b626cb55c95ae55f34f246023
ryuseibs <beryusei0923212ssyo@aol.com>
2025-03-18 20:49:07 +0900

    Song データクラスを修正(filePath に「実際の音楽ファイルのパス」を格納)

--- app/src/main/java/com/example/music_player/Song.kt
+++ app/src/main/java/com/example/music_player/Song.kt
@@ -3,5 +3,5 @@ package com.example.music_player
 data class Song (
     val title: String,      // タイトル
     val artist: String,     // アルバム
-    val albumArtResId: Int  // アルバムアートの画像リソースID
+    val filePath: String  // アルバムアートの画像リソースID
 )
